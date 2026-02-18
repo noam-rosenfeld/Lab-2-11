cbc1f33bb5b5060cef99428e3623bc6e680ebb95
Noam Rosenfeld <30053513@cc.peralta.edu>
2026-02-18 20:17:05 +0000

    progress_1

---
+++ MinMaxFinder.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int numbers[] = {23, 5, 67, -2, 45, 87, 1, 99, 34, -10};
+    int min = numbers[0];
+    int max = numbers[0];
+
+    for(int i = 1; i < size_of(numbers); i++) {
+        if(numbers[i] > max) {
+            max
+        }
+    }
+}
