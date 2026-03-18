154f9bf7922fc08cccdb146e5a5131fa2ad8b0a0
Noam Rosenfeld <30053513@cc.peralta.edu>
2026-03-18 19:15:31 +0000

    MinMax completed

--- MinMaxFinder.cpp
+++ MinMaxFinder.cpp
@@ -2,14 +2,25 @@
 
 using namespace std;
 
+void findMinAndMax(const int arr[], int size, int* min, int* max) {
+    for(int i = 1; i < size; i++) {
+        if(arr[i] > *max) {
+            *max = arr[i];
+        }
+
+        if(arr[i] < *min) {
+            *min = arr[i];
+        }
+    }
+}
+
 int main() {
-    int numbers[] = {23, 5, 67, -2, 45, 87, 1, 99, 34, -10};
+    const int numbers[] = {23, 5, 67, -2, 45, 87, 1, 99, 34, -10};
     int min = numbers[0];
     int max = numbers[0];
 
-    for(int i = 1; i < size_of(numbers); i++) {
-        if(numbers[i] > max) {
-            max
-        }
-    }
+    findMinAndMax(numbers, 10, &min, &max);
+
+    cout << "The minimum value is: " << min << endl;
+    cout << "The maximum value is: " << max << endl;
 }
